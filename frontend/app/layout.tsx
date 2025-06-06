3a1b7ec9f93e91c80d63ed1c5554cc82d36234d1
Kacper <kacperjozwik12@gmail.com>
2025-06-06 21:40:41 +0200

    Refactor code structure for improved readability and maintainability

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -10,52 +10,52 @@ import { TokenBalanceProvider } from '@/lib/contexts/TokenBalanceContext';
 const sizmoPro = localFont({
   src: [
     {
-      path: '../public/fonts/SizmoPro-Light.woff2',
+      path: './public/fonts/SizmoPro-Light.woff2',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '../public/fonts/SizmoPro-LightOblique.woff2',
+      path: './public/fonts/SizmoPro-LightOblique.woff2',
       weight: '300',
       style: 'italic',
     },
     {
-      path: '../public/fonts/SizmoPro.woff2',
+      path: './public/fonts/SizmoPro.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../public/fonts/SizmoPro-Oblique.woff2',
+      path: './public/fonts/SizmoPro-Oblique.woff2',
       weight: '400',
       style: 'italic',
     },
     {
-      path: '../public/fonts/SizmoPro-Medium.woff2',
+      path: './public/fonts/SizmoPro-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../public/fonts/SizmoPro-MediumOblique.woff2',
+      path: './public/fonts/SizmoPro-MediumOblique.woff2',
       weight: '500',
       style: 'italic',
     },
     {
-      path: '../public/fonts/SizmoPro-Demibold.woff2',
+      path: './public/fonts/SizmoPro-Demibold.woff2',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '../public/fonts/SizmoPro-DemiboldOblique.woff2',
+      path: './public/fonts/SizmoPro-DemiboldOblique.woff2',
       weight: '600',
       style: 'italic',
     },
     {
-      path: '../public/fonts/SizmoPro-Bold.woff2',
+      path: './public/fonts/SizmoPro-Bold.woff2',
       weight: '800',
       style: 'normal',
     },
     {
-      path: '../public/fonts/SizmoPro-BoldOblique.woff2',
+      path: './public/fonts/SizmoPro-BoldOblique.woff2',
       weight: '800',
       style: 'italic',
     },
