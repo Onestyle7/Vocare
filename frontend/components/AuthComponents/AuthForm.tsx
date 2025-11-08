47bb5345def15313c9f4a1cc26f0f0ad6f6a15fb
Kacper <kacperjozwik12@gmail.com>
2025-11-08 12:08:27 +0100

    fix font

--- frontend/components/AuthComponents/AuthForm.tsx
+++ frontend/components/AuthComponents/AuthForm.tsx
@@ -177,7 +177,7 @@ const AuthForm = ({ type }: AuthFormProps) => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form font-korbin">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form font-poppins">
         <h1 className="form-title">{isSignUp ? 'Sign Up' : 'Sign In'}</h1>
 
         {isSignUp && (
