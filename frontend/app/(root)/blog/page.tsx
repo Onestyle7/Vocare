6e8fa3c634da5d4a84f7e8da2758810be0ac4f58
Kacper <kacperjozwik12@gmail.com>
2025-11-22 17:01:36 +0100

    feat: add blog page with construction notice and navigation link

--- frontend/app/(root)/blog/page.tsx
+++ frontend/app/(root)/blog/page.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import { construction } from '@/app/constants'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
 
-const page = () => {
-  return <div>page</div>;
-};
+const blog = () => {
+  return (
+    <div className=' font-korbin h-screen flex items-center justify-center max-w-7xl mx-auto mt-10'>
+      <Link href='/' className='absolute top-8 left-8 text-gray-500 hover:text-gray-400'>
+        &larr; Back to Home
+      </Link>
+      <div className='flex flex-col items-center justify-center border border-r-6 border-b-6 p-4 rounded-[24px]'>
+      <h1 className='text-xl text-center mb-4'>
+        We are currently working on this section. <br />Stay tuned.
+      </h1>
+      <Image src={construction} className='border border-r-6 border-b-6 rounded-[16px]' alt='construction' width={424} height={424}/>
+    </div>
+  </div>
+  )
+}
 
-export default page;
+export default blog
